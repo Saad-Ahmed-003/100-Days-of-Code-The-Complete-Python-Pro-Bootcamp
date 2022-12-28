a0afffcba51b396d50c37a3bb3d4f6682427be21
saad ahmed bin muhammed sajid pasha <117589869+Saad-Ahmed-003@users.noreply.github.com>
2022-12-28 19:41:11 +0530

    Update main.py

--- Day_16 intermediate/main.py
+++ Day_16 intermediate/main.py
@@ -1,4 +1,16 @@
 from prettytable import PrettyTable
 
-table = PrettyTable()
+table = PrettyTable(["name", "type"])
+
+table.align["name"] = "l"
+table.align["type"] = "l"
+table.padding_width = 1
+
+table.add_row(["Chesnaught", "grass, fighting"])
+table.add_row(["Greninja", "Water, dark"])
+table.add_row(["Pikachu", "Electric"])
+table.add_row(["Venusaur", "Grass, poison"])
+table.add_row(["Charizard", "Fire, flight"])
+table.add_row(["Blastoise", "Water"])
+
 print(table)
