0a6b1e25496f22ed98a8302ca0acb55ad64b38e7
saad ahmed bin muhammed sajid pasha <117589869+Saad-Ahmed-003@users.noreply.github.com>
2022-12-28 20:25:41 +0530

    renameing

---
+++ DAY_2.py
@@ -0,0 +1,12 @@
+#If the bill was $150.00, split between 5 people, with $1.12 tip.
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the resultto 2 ways to round a number. you might have to do some googling to solve this.💪
+
+#Write your code below this line 👇
+
+data = input("type your amount")
+
+processedData = (data / 5) * 1.12
+
+print(processedData)
