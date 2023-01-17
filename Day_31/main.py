3c391c52b3e973ce7c9f342dbaf7572002f37dea
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-01-18 02:29:59 +0530

    new

--- Day_31/main.py
+++ Day_31/main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -7,7 +7,7 @@ YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+LONG_BREAK_MIN = 35
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -16,3 +16,15 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+
+window.mainloop()
