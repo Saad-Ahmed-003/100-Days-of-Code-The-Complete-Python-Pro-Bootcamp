55a8ef16683f18d72cc5836fc3986717e2fe5d77
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-01-24 18:08:45 +0530

    Update main.py

--- Day_32/main.py
+++ Day_32/main.py
@@ -12,7 +12,7 @@ window.config(padx=20, pady=20)
 canvas = Canvas(height=208, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack() 
+canvas.pack()
 
 
 
