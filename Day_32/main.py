4f5ae751381a8d6b7d5902986decd3f1688dbd74
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-01-24 23:43:41 +0530

    Update main.py

--- Day_32/main.py
+++ Day_32/main.py
@@ -12,9 +12,26 @@ window.config(padx=20, pady=20)
 canvas = Canvas(height=208, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
 
+# Labels
+website_label = Label(text="Website")
+website_label.grid(row=1, column=0)
+email_label = Label(text="E-mail/Username")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password")
+password_label.grid(row=3, column=0)
 
+# Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+# Buttons
+genrate_apssword_button = B
 
 
 window.mainloop()
