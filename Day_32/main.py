38c5accd70fe672bc393fb52fb0d688c3466db32
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-01-25 20:36:24 +0530

    Update main.py

--- Day_32/main.py
+++ Day_32/main.py
@@ -7,7 +7,7 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(height=208, width=200)
 logo_img = PhotoImage(file="logo.png")
@@ -24,14 +24,18 @@ password_label.grid(row=3, column=0)
 
 # Entries
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1)
+website_entry.grid(row=1, column=1, columnspan=2)
 email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1)
-password_entry = Entry(width=21)
+email_entry.grid(row=2, column=1, columnspan=2)
+password_entry = Entry(width=20)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = Button(text=)
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36)
+add_button.grid(row=4, column=1, columnspan=2)
+
 
 
 window.mainloop()
