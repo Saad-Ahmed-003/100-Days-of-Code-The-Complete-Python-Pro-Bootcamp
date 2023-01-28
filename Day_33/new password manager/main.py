46700877f8f0ef5fefd8a1e071b745105a48b0c6
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-01-28 18:17:54 +0530

    a

--- Day_33/new password manager/main.py
+++ Day_33/new password manager/main.py
@@ -45,48 +45,79 @@ def save():
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
                                                               f""f"\nPassword: {password} \nIs it ok to save?")
         if is_ok:
-            with open("data.json", "r") as data_file:
-                data = json.load(data_file)
+            try:
+                with open("data.json", "r") as data_file:
+                    data = json.load(data_file)
+                    data.update(new_data)
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
                 data.update(new_data)
-            with open("data.json", "w") as data_file:
-                json.dump(data, data_file, indent=4)
+                with open("data.json", "w") as data_file:
+                    json.dump(data, data_file, indent=4)
+            finally:
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
 
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title=website, message="File not found!")
+            if website in data:
+                email = data[website]["email"]
+                password = data[website]["password"]
+                messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+                password_entry.insert(0, password)
+                pyperclip.copy(password)
+
+
+
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=50, pady=50)
+window.config(padx=50, pady=50, bg="#222222")
 
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
+canvas.config(bg="#222222", highlightthickness=0)
 
 # Labels
-website_label = Label(text="Website:")
+website_label = Label(text="Website:", bg="#222222", fg="#E6DDC4")
 website_label.grid(row=1, column=0)
-email_label = Label(text="Email/Username:")
+email_label = Label(text="Email/Username:", bg="#222222", fg="#E6DDC4")
 email_label.grid(row=2, column=0)
-password_label = Label(text="Password:")
+password_label = Label(text="Password:", bg="#222222", fg="#E6DDC4")
 password_label.grid(row=3, column=0)
 
 # Entries
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.grid(row=2, column=1)
 email_entry.insert(0, "angela@gmail.com")
-password_entry = Entry(width=21)
+password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = Button(text="Generate Password", command=generate_password)
+search_button = Button(text="Search", bg="#3C4048", fg="#E6DDC4", highlightthickness=0, command=find_password)
+search_button.grid(row=1, column=2)
+generate_password_button = Button(text="Generate Password", command=generate_password, bg="#3C4048", fg="#E6DDC4",
+                                  highlightthickness=0)
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
+add_button = Button(text="Add", width=36, command=save, bg="#3C4048", fg="#E6DDC4", highlightthickness=0)
+add_button.grid(row=4, column=1,)
 
 window.mainloop()
