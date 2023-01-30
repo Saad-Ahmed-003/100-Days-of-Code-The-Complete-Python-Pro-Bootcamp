bc19aa85934b7ff1e747fbcf60adc2f0c36e217a
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-01-31 00:03:33 +0530

    a

--- Day_26/us-states-game-end/main.py
+++ Day_26/us-states-game-end/main.py
@@ -9,7 +9,7 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
-print(all_states)
+print(data)
 guessed_states = []
 
 while len(guessed_states) < 50:
--- Day_36/extra hard/main.py
+++ Day_36/extra hard/main.py
@@ -3,4 +3,19 @@ import datetime as dt
 import random
 import pandas
 
+data = pandas.read_csv("birthdays.csv")
+
+days = data.day.to_list()
+months = data.month.to_list()
+years = data.year.to_list()
+Emails = data.email.to_list()
+names = data.name.to_list()
+
+
+
+
+
+
+
+
 
