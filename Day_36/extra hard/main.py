e6a14f1f9c4c6681566f736aee948bd9ee81494b
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-01-31 01:56:57 +0530

    Update main.py

--- Day_36/extra hard/main.py
+++ Day_36/extra hard/main.py
@@ -3,6 +3,9 @@ import datetime as dt
 import random
 import pandas
 
+num = 0
+num_1 = 0
+
 data = pandas.read_csv("birthdays.csv")
 
 days = data.day.to_list()
@@ -11,9 +14,16 @@ years = data.year.to_list()
 Emails = data.email.to_list()
 names = data.name.to_list()
 
-
-
-
+dict_1 = data.to_dict()
+print(dict_1)
+for i in dict_1:
+    for x in dict_1[i]:
+        print(num_1)
+    if num > 5:
+        num_1 = 0
+    elif num < 4:
+        num_1 = 1
+    num += 1
 
 
 
