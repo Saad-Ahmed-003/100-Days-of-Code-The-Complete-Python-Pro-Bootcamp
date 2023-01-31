58c59cc8d68d993f90749eadf567e4a9257eb5ad
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-01-31 14:59:31 +0530

    a

--- Day_36/extra hard/main.py
+++ Day_36/extra hard/main.py
@@ -6,14 +6,24 @@ import pandas
 data = pandas.read_csv("birthdays.csv")
 
 dict_1 = data.to_dict()
+repeat_range = data.index.stop
 
+num = 0
+list_1 = []
+list_2 = []
 
+while repeat_range != num:
+    for i in dict_1:
+        for x in dict_1[i]:
+            if x == num:
+                list_1.append(dict_1[i][x])
+                if len(list_1) == 5:
+                    list_2.append(list_1)
+                    list_1 = []
+    num += 1
 
-for i in dict_1:
-    for x in dict_1[i]:
-        print(x)
-
-
+if 22 in data.day:
+    print("fuck")
 
 
 
