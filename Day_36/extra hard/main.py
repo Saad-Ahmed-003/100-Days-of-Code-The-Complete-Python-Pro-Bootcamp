6b20972c03ef6b35bd2e2fbd6876bead132bcf43
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-01-31 23:49:07 +0530

    a

--- Day_36/extra hard/main.py
+++ Day_36/extra hard/main.py
@@ -22,6 +22,10 @@ list_3 = [x for x in dict_1]
 list_4 = [i for i in range(repeat_range)]
 new_dict = {}
 
+PLACEHOLDER = "[NAME]"
+letter_file = ["letter_1", "letter_2", "letter_3"]
+
+
 while repeat_range != num:
     for i in dict_1:
         for x in dict_1[i]:
@@ -30,11 +34,20 @@ while repeat_range != num:
                 if len(list_1) == 5:
                     dict_2 = {k: v for (k, v) in zip(list_3, list_1)}
                     list_1 = []
-    new_dict[num] = dict_2
+    new_dict[str(num)] = dict_2
     num += 1
 
 
-print(month, date)
-
-
-
+for i in new_dict:
+    if month == new_dict[i]['month']:
+        if date == new_dict[i]['day']:
+            with open(f"letter_templates/{random.choice(letter_file)}.txt") as letter:
+                letter_content = letter.read()
+                new_letter = letter_content.replace(PLACEHOLDER, new_dict[i]['name'])
+                with smtplib.SMTP("smtp.gmail.com") as connection:
+                    connection.starttls()
+                    connection.login(user=my_email, password=My_password)
+                    connection.sendmail(from_addr=my_email,
+                                        to_addrs=new_dict[i]['email'],
+                                        msg=f"Subject:Happy BIRTHDAY!\n\n"
+                                            f"{new_letter}")
