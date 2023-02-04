93c94ed03c963a4645647a59cb5e14ffbf22eb21
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-02-04 23:46:55 +0530

    Update main.py

--- Day_37/project 2/main.py
+++ Day_37/project 2/main.py
@@ -1,11 +1,15 @@
 import requests
 from datetime import datetime
+import smtplib
+import time
 
-MY_LAT = 51.507351  # Your latitude
-MY_LONG = -0.127758  # Your longitude
+MY_EMAIL = "___YOUR_EMAIL_HERE____"
+MY_PASSWORD = "___YOUR_PASSWORD_HERE___"
+MY_LAT = 51.507351 # Your latitude
+MY_LONG = -0.127758 # Your longitude
 
 
-def check_position():
+def is_iss_overhead():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
     data = response.json()
@@ -13,12 +17,9 @@ def check_position():
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
 
+    #Your position is within +5 or -5 degrees of the iss position.
     if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
         return True
-    else:
-        return False
-
-# Your position is within +5 or -5 degrees of the ISS position.
 
 
 def is_night():
@@ -33,13 +34,22 @@ def is_night():
     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-time_now = datetime.now()
+    time_now = datetime.now().hour
 
-print()
+    if time_now >= sunset or time_now <= sunrise:
+        return True
 
-# If the ISS is close to my current position, and it is currently dark
-# Then email me to tell me to look up.
-# BONUS: run the code every 60 seconds.
 
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        connection = smtplib.SMTP("__YOUR_SMTP_ADDRESS_HERE___")
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg="Subject:Look Up👆\n\nThe ISS is above you in the sky."
+        )
 
 
