1b5711033dc3636447111f4f2af14d4bb6b2c638
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-02-07 13:20:47 +0530

    a

--- Day_38/data.py
+++ Day_38/data.py
@@ -1,6 +1,11 @@
 import requests
 
-response = requests.get(url="https://opentdb.com/api.php?amount=10&type=boolean")
+parameters = {
+    "amount": 10,
+    "type": "boolean",
+}
 
+response = requests.get("https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
 data = response.json()
 question_data = data["results"]
