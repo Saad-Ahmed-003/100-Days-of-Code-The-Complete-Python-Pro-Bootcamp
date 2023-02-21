d17c67554d725aa870b000b8d7a9cb74c34ef699
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-02-22 00:40:38 +0530

    Create main.py

---
+++ Day_41/main.py
@@ -0,0 +1,10 @@
+import requests
+
+
+App_ID = "7fb4819c"
+
+API_Key = "03df4f0a1aa6b98b860772c4832fe8a6	—"
+
+nutritionix_API = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+sheety_API = "https://api.sheety.co/7fda9457fd9c8184947c825a52944bf6/myWorkouts/workouts"
