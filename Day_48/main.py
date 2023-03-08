3db238dd3d96dd41c71b44a0e4806a104e5db057
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-03-09 01:54:54 +0530

    Update main.py

--- Day_48/main.py
+++ Day_48/main.py
@@ -10,7 +10,7 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get("http://www.python.org")
 
-content1 = driver.find_elements(By.CSS_SELECTOR, ".event-widget ul.menu li time")
+content1 = driver.find_elements(By.CLASS_NAME, ".event-widget ul.menu li time")
 content2 = driver.find_elements(By.CSS_SELECTOR, ".event-widget ul.menu li a")
 
 # for i in range(len(list1)):
