9e085ca50bd87df8bb7769bc81545c9f9e7b2ad7
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-03-13 23:41:48 +0530

    Update main.py

--- Day_57/New folder/main.py
+++ Day_57/New folder/main.py
@@ -4,16 +4,19 @@ import requests
 
 app = Flask(__name__)
 
+response = requests.get(url="https://api.npoint.io/427bb5220a9f9944117b")
+all_posts = response.json()
 
-@app.route('/')
+
+@app.route('/blog')
 def home():
-    response = requests.get(url="https://api.npoint.io/427bb5220a9f9944117b")
-    all_posts = response.json()
     return render_template("index.html", posts=all_posts)
 
-@app.route('1')
-def post():
-    
+
+@app.route('/post/<int:id_num>')
+def post(id_num):
+    return render_template("post.html", posts=all_posts, id=id_num)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
