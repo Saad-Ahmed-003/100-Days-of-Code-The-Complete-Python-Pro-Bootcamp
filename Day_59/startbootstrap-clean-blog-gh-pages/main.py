6d12aa0aa47420fc95b561e08eb431fd760ee064
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-03-14 15:02:11 +0530

    Update main.py

--- Day_59/startbootstrap-clean-blog-gh-pages/main.py
+++ Day_59/startbootstrap-clean-blog-gh-pages/main.py
@@ -8,5 +8,20 @@ def home():
     return render_template("index.html")
 
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/post')
+def post():
+    return render_template("post.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
