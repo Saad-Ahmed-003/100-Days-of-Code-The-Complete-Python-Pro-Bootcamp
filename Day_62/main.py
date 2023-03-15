b6b5ca04f2540b504ad35c5508117be7138e1321
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-03-15 16:36:07 +0530

    Update main.py

--- Day_62/main.py
+++ Day_62/main.py
@@ -8,5 +8,10 @@ def home():
     return render_template('index.html')
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
